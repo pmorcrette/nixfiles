827579affca9d63ea2a4fda56b03acfb073854bf
Pierre Morcrette <pmorcrette@pm.me>
2023-03-01 14:05:39 +0100

    Moving zsh plugins management to zplug
     On branch master
     Changes to be committed:
    	modified:   zsh.nix

--- zsh.nix
+++ zsh.nix
@@ -20,23 +20,11 @@
      TERM=xterm
      source ~/.zfunc/_awsh
                               '';
-
-    plugins = with pkgs; [
-      {
-        name = "zsh-syntax-highlighting";
-        src = fetchFromGitHub {
-          owner = "zsh-users";
-          repo = "zsh-syntax-highlighting";
-          rev = "0.6.0";
-          sha256 = "0zmq66dzasmr5pwribyh4kbkk23jxbpdw4rjxx0i7dx8jjp2lzl4";
-        };
-        file = "zsh-syntax-highlighting.zsh";
-      }
-    ];
     zplug = {
       enable = true;
       plugins = [
         { name = "greymd/docker-zsh-completion"; }
+        { name = "zsh-users/zsh-syntax-highlighting"; }
       ];
     };
   };
